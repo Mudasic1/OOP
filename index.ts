81bbba1afd8273289c4a0d548e5f4ca9044ce933
Muhammad Mudasir Chandio <chandiommudasir084@gmail.com>
2024-07-03 02:53:47 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,52 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+// Create class
+class Person {
+    private _personality: string;
+
+    constructor() {
+        this._personality = 'Mystery';
+    }
+
+    // User Input
+    userInput(input: string) {
+        // if else statements
+        if (input.includes('talk to other about yourself openly')) {
+            this._personality = 'talk happily';
+        } else if (input.includes('keep your word to yourself')) {
+            this._personality = 'talk rude';
+        }
+    }
+    // return function
+get personality() {
+    return this._personality;
+}
+
+}
+class Main {
+    public async main() {
+      // await called out
+      const ans = await inquirer.prompt({
+        // object create
+        type: 'list',
+        name: 'choice',
+        message: chalk.yellow('tell your behaviour ?'),
+        choices: [
+            "1. talk to others about myself. ",
+            "2. keep quite and don't ask question "
+        ],
+      });
+      //create a personal class
+let myPerson = new Person();
+
+myPerson.userInput(ans.choice);
+
+//output response
+console.log('you are ' + myPerson.personality);
+    }
+  }
+  
+  const myObject = new Main();
+  myObject.main();
+  
